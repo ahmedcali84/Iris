e43ca9d4e4dfd56dc1dd8f84c5967d4a5fd62d14
ahmedcali84 <107881879+ahmedcali84@users.noreply.github.com>
2024-05-10 19:31:43 +0300

    Add tests and changes to templates and static files

--- irisApp/tests.py
+++ irisApp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
+from .utils import loadModel
 
 # Create your tests here.
+class IrisTestCase(TestCase):
+	
+	def setUp(self) -> None:
+		User.objects.create(username="axmed", email="axmed@m.com")
+		
+	def test_prediction_1(self):
+		model = loadModel()
+		predict = model.predict([[2,3,4,5]])
+		user = User.objects.get(username="axmed")
+		IrisModel.objects.create(user = user , sepal_length = 2, sepal_width = 3, petal_length = 4, petal_width = 5, prediction = "setosa")
+		iris = IrisModel.objects.filter(user=user).first()
+		print(iris.prediction , predict)
+		self.assertFalse(str(iris.prediction) == str(predict[0]))
+		
